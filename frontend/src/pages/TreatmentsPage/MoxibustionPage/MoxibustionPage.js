342da79a5fe76aaa15d55be639c0393d476d1226
Derek <derekbohman@gmail.com>
2022-08-06 15:45:45 -0600

    Completed css for moxibustion page.

--- frontend/src/pages/TreatmentsPage/MoxibustionPage/MoxibustionPage.js
+++ frontend/src/pages/TreatmentsPage/MoxibustionPage/MoxibustionPage.js
@@ -1,11 +1,45 @@
 import "./MoxibustionPage.css";
+import moxibustion3 from "../SupportingDocuments/moxibustion3.jpg";
+import moxibustion4 from "../SupportingDocuments/moxibustion4.jpg";
 import React from "react";
 
 const MoxibustionPage = () => {
   return (
     <div className="moxibustionContainer">
-      <h1>Moxibustion</h1>
-      <div className="moxibustionSpacer"></div>
+      <div className="moxibustionContent">
+        <div className="moxibustionBannerContainer"></div>
+        <h1>Moxibustion</h1>
+        <h2>What is moxibustion?</h2>
+        <img src={moxibustion3} className="moxibustion3" />
+        <p>
+          Mugwort, or artemisia, is a flowering plant in the daisy family. It’s
+          used in foods eaten around the world, including German Christmas
+          goose, Korean clam soup and a green Japanese dessert known as kusa
+          mochi.
+        </p>
+        <p>
+          It’s also the key to moxibustion. Mugwort leaves are dried and ground
+          into small sticks or cones called moxa (similar to incense), which are
+          then lighted and used to warm points on your body and relieve a
+          variety of conditions. In TCM, the concepts of yin and yang balance
+          each other, but imbalances can lead to illness.
+        </p>
+        <h2>How is moxibustion done?</h2>
+        <img src={moxibustion4} className="moxibustion4" />
+        <p>
+          Direct moxibustion is when your practitioner places moxa cones
+          directly onto your skin. More common in the United States, though, is
+          indirect moxibustion, when the burning moxa doesn’t directly touch
+          your body.
+        </p>
+        <h2>Does moxibustion hurt?</h2>
+        <p>
+          It shouldn’t! With indirect moxibustion, the moxa never touches your
+          skin. And even with direct moxa, you should only feel heat and warmth,
+          but not pain; your practitioner should remove the moxa before it burns
+          or blisters.
+        </p>
+      </div>
     </div>
   );
 };
